d1c5727d1b4b33f5c7743ef8c035b836e472781f
doutatsu <doutatsu@gmail.com>
2020-09-12 16:12:25 +0100

    Parser takes in cli arguments
    - If file path is not specified, print error message
    and quit
    - Check if mode is specified, otherwise default to
    most_unique_visited
    - Print out the results at the end of the script

--- parser.rb
+++ parser.rb
@@ -1,1 +1,13 @@
 # frozen_string_literal: true
+
+require './log_parser'
+
+unless ARGV[0]
+  puts 'Please provide file path'
+  return
+end
+
+file = ARGV[0]
+mode = ARGV[1].nil? ? :most_unique_visited : ARGV[1].to_sym
+
+puts LogParser.parse(file, mode).join("\n")
