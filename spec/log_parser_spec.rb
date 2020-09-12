510b8e7893a76036c907371f8560387fc826827c
doutatsu <doutatsu@gmail.com>
2020-09-12 16:11:19 +0100

    Move parsing logic from Parser into LogParser
    Parser will take in cli arguments, so it's better to
    separate the logic, which will also make unit testing
    easier

---
+++ spec/log_parser_spec.rb
@@ -0,0 +1,39 @@
+require './log_parser'
+
+RSpec.describe LogParser do
+  describe 'when file is provided' do
+    describe 'and mode is most_unique_visited' do
+      it 'returns list of webpages with most unique page views, ordered from most to least visited' do
+        output = described_class.parse('webserver.log', :most_unique_visited)
+
+        expect(output).to match_array(
+          [
+            '/help_page/1 7 unique visits',
+            '/contact 4 unique visits',
+            '/about 4 unique visits',
+            '/home 4 unique visits',
+            '/about/2 3 unique visits',
+            '/index 1 unique visits'
+          ]
+        )
+      end
+    end
+
+    describe 'and mode is most_visited' do
+      it 'returns list of webpages with most page views ordered from most to least visited' do
+        output = described_class.parse('webserver.log', :most_visited)
+
+        expect(output).to match_array(
+          [
+            '/about/2 90 visits',
+            '/contact 89 visits',
+            '/index 82 visits',
+            '/about 81 visits',
+            '/help_page/1 80 visits',
+            '/home 78 visits'
+          ]
+        )
+      end
+    end
+  end
+end
